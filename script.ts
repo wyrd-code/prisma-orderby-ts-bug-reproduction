4d346674d19be042a68ec7665927718760551fab
Tomislav Glavaš <tomo.glavas@gmail.com>
2021-09-14 19:47:55 +0200

    Demonstrate orderBy typescript bug

--- script.ts
+++ script.ts
@@ -4,7 +4,18 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+    orderBy: [
+      {
+        email: 'desc',
+      },
+      {
+        name: 'desc',
+      },
+    ]
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
